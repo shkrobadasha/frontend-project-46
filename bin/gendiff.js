dd53a04de1afeb5dbb46b3d80bff62e46b137b13
Shkroba Daria <shkrobadaria@mail.ru>
2024-09-29 20:03:58 +0300

    add --f option

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,6 +5,9 @@ import { program } from 'commander';
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
-  .version('0.8.0');
+  .version('0.8.0')
+  .arguments('<filepath1> <filepath2>')
+  .option('-f, --format [type]', 'output format');
+  
 
 program.parse();
