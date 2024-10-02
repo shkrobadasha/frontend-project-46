26fc47cdf4d55f81d19e31f47df607241469f763
Shkroba Daria <shkrobadaria@mail.ru>
2024-10-02 11:52:46 +0300

    step 4 is completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,13 +1,26 @@
 #!/usr/bin/env node
 
 import { program } from 'commander';
+import { letsParse } from '../src/index.js';
+import { createRequire } from "module";
+import { genDiff } from '../src/index.js';
+const require = createRequire(import.meta.url);
+const fs = require('fs');
 
 program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('0.8.0')
   .arguments('<filepath1> <filepath2>')
-  .option('-f, --format [type]', 'output format');
-  
+  .option('-f, --format [type]', 'output format')
+  .action((first, second) => {
+    const obj1 = letsParse(fs.readFileSync(first));
+    const obj2 = letsParse(fs.readFileSync(second));
+    console.log(genDiff(obj1, obj2))
+
+    //здесь будем вызывать функцию, которая читает названия файлов, 
+    //находит сами файлы, парсит их и печатате первую строку
+    
+  });
 
 program.parse();
--- src/index.js
+++ src/index.js
@@ -1,7 +1,69 @@
-//у нас будут парситься данные из файлов
-//мы передадим названия как аргументы и должны найти их пути, чтобы взять эти файлы и распарсить(?)
+import { cwd } from 'node:process';
+import { createRequire } from "module";
+import _ from 'lodash';
 
-//мы получаем путь, читаем файл
+const require = createRequire(import.meta.url);
+const path = require('path');
 
-const obj1 = JSON.parse(str1);
-const obj2 = JSON.parse(str2);
+export const letsParse = (readingFile) => JSON.parse(readingFile);
+
+
+export const normalName = (fileName) => {
+    const pathOfFile = fileName;
+    if (!fileName.startsWith('/')) {
+        const currentDirectory = cwd();
+        console.log(currentDirectory)
+        return path.resolve(`${currentDirectory}`, fileName);
+    }
+    return pathOfFile;
+}
+
+export const genDiff = (obj1, obj2) => {
+    const keys1 = Object.keys(obj1);
+    const keys2 = Object.keys(obj2);
+    const keys = _.union(keys1, keys2); //уникальные ключи
+  
+    const result = {};
+    for (const key of keys) {
+      if (!obj1.hasOwnProperty(key)) {
+        result['+ '+`${key}`] = obj2[key]; //+
+      
+      } else if (!obj2.hasOwnProperty(key)) {
+        result['- '+`${key}`] = obj1[key]; //-
+      } else if (obj1[key] !== obj2[key]) {
+        result['- '+`${key}`] = obj1[key];
+        result['+ '+`${key}`] = obj2[key];
+      } else {
+        result[key] = obj1[key];
+      }
+    }
+    
+    const totalResult = Object.entries(result);
+    totalResult.sort((a, b) => {
+      let nameA = (a[0].startsWith('+') || a[0].startsWith('-')) ? a[0].slice(2) : a[0]
+      let nameB = (b[0].startsWith('+') || b[0].startsWith('-')) ? b[0].slice(2) : b[0]
+    
+      if (nameA < nameB) {
+        return -1;
+      }
+      if (nameA > nameB) {
+        return 1;
+      }
+      return 0;
+    });
+    
+    return getFormattedOutput(totalResult)
+  }
+
+const getFormattedOutput = (arr) => {
+  let result = '{\n';
+  arr.forEach((item) => {
+    if(!(item[0].startsWith('+') || item[0].startsWith('-'))){
+      result += `    ${item[0]}: ${item[1]}\n`
+    } else {
+      result += `  ${item[0]}: ${item[1]}\n`
+    }
+  });
+  result += '}'
+  return result
+};
