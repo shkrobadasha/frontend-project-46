a37f1ca691a450f0dcf86437090add6355abd416
Shkroba Daria <shkrobadaria@mail.ru>
2024-10-25 21:05:50 +0300

    fix json

--- formatters/json.js
+++ formatters/json.js
@@ -1,36 +1,6 @@
-import _ from 'lodash';
-import sortAlphabetically from './util.js';
+const json = (result) => {
+  const newRes = JSON.stringify(result, null, 2);
 
-const formatValue = (value) => {
-  if (typeof value === 'string') {
-    return `"${value}"`;
-  }
-  return value;
+  return newRes;
 };
-
-const json = (tree) => {
-  let res = '{\n';
-
-  const iter = (node, depth) => {
-    const pairs = Object.entries(node);
-
-    sortAlphabetically(pairs).forEach(([key, value]) => {
-      const shift = ((key.startsWith('+ ') || key.startsWith('- '))) ? 2 : 0;
-      res += `${' '.repeat(4 * depth - shift)}"${(key)}": `;
-      if (!_.isObject(value)) {
-        res += `${formatValue(value)},\n`;
-        return res;
-      }
-
-      res += '{\n';
-      return iter(value, depth + 1);
-    });
-    res += `${' '.repeat(4 * (depth - 1))}}\n`;
-
-    return res;
-  };
-
-  return iter(tree, 1);
-};
-
 export default json;
