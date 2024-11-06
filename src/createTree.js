62ff3078f3ee40612f3a8f81e8b62ea0ad7683e0
Shkroba Daria <shkrobadaria@mail.ru>
2024-11-06 23:40:34 +0300

    final fix

--- src/createTree.js
+++ src/createTree.js
@@ -2,8 +2,7 @@ import _ from 'lodash';
 
 const findDifferences = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Object.keys(obj2)))
   .map((key) => {
-    if (Object.prototype.hasOwnProperty.call(obj1, key)
-    && Object.prototype.hasOwnProperty.call(obj2, key)) {
+    if (_.has(obj1, key) && _.has(obj2, key)) {
       if (_.isObject(obj1[key]) && _.isObject(obj2[key])) {
         return { nodeName: key, children: findDifferences(obj1[key], obj2[key]), type: 'nested' };
       }
@@ -13,10 +12,10 @@ const findDifferences = (obj1, obj2) => _.sortBy(_.union(Object.keys(obj1), Obje
         };
       }
     }
-    if (!Object.prototype.hasOwnProperty.call(obj1, key)) {
+    if (!_.has(obj1, key)) {
       return { nodeName: key, type: 'add', value: obj2[key] };
     }
-    if (!Object.prototype.hasOwnProperty.call(obj2, key)) {
+    if (!_.has(obj2, key)) {
       return { nodeName: key, type: 'remove', value: obj1[key] };
     }
     return { nodeName: key, type: 'unchanged', value: (obj1[key]) };
--- src/parsers.js
+++ src/parsers.js
@@ -2,14 +2,14 @@ import * as yaml from 'js-yaml';
 import { getFormat, getContentOfFile } from './util.js';
 
 const parseOfFormat = {
-  '.json': (nameOfFile) => JSON.parse(getContentOfFile(nameOfFile)),
-  '.yml': (nameOfFile) => yaml.load(getContentOfFile(nameOfFile)),
-  '.yaml': (nameOfFile) => yaml.load(getContentOfFile(nameOfFile)),
+  json: (nameOfFile) => JSON.parse(getContentOfFile(nameOfFile)),
+  yml: (nameOfFile) => yaml.load(getContentOfFile(nameOfFile)),
+  yaml: (nameOfFile) => yaml.load(getContentOfFile(nameOfFile)),
 };
 
 const letsParse = (nameOfFile) => {
-  const format = getFormat(nameOfFile);
-  if (!Object.keys(parseOfFormat).includes(format)) {
+  const format = getFormat(nameOfFile).slice(1);
+  if (parseOfFormat[format] === undefined) {
     throw new Error('No current format');
   }
   return parseOfFormat[format](nameOfFile);
